0c16210a3f161be2b96fe2483f3e289441cf60e4
Aniket Singla <imaniketsingla8@gmail.com>
2025-06-24 00:07:53 +0530

    added user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    changePassword,
+    refreshAccessToken 
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout",verifyJWT, logoutUser);
+router.post("/change-password",verifyJWT, changePassword);
+router.post("/refresh-token", refreshAccessToken);
 
 export default router;
