2b211c0b0c98fb04b49cc597c27a835749571265
Aniket Singla <imaniketsingla8@gmail.com>
2025-06-24 00:16:37 +0530

    add user routes to the main application

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from '../db/index.js';
 import attendanceRoutes from '../routes/attendanceRoutes.js';
+import userRoutes from '../routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.use('/api/attendance', attendanceRoutes);
+app.use('/api/users', userRoutes);
 
 dotenv.config({
     path: '../env',
